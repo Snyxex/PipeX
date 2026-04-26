078e4a9012f39cd7854e5fe1b69a8067ac22c9d5
snyxe <153672410+Snyxex@users.noreply.github.com>
2026-04-26 11:58:08 +0200

    Revert "git init"
    This reverts commit 24ecbdfd066aa0b0865432b84dbb2a46a292d0f4.

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-// Export the Core
-export * from './core/core';
-export * from './core/types';
-
-
-export { HashingPlugin } from './plugin/hashing';
-export { EncryptionPlugin } from './plugin/encryption';
-export { CompressionPlugin } from './plugin/compression';
